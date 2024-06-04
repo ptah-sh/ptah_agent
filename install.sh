0bd3aa0e39a7993e6a85c7cede614d3c291065e4
Bohdan Shulha <b.shulha@pm.me>
2024-06-04 19:45:00 +0200

    fix: #5 path typos

--- install.sh
+++ install.sh
@@ -156,9 +156,9 @@ BASHRC
 
 curl -L https://github.com/ptah-sh/ptah_agent/releases/latest/download/ptah_agent_linux_x86_64.tar.xz -o /tmp/ptah_agent_latest.tar.xz
 
-mkdir -p \$HOME/ptah_agent/versions
+mkdir -p \$HOME/ptah_agent/versions/0_seed
 
-tar -xvJf /tmp/ptah_agent_latest.tar.xz -C \$HOME/ptah_agent/versions/0_seed/ptah_agent
+tar -xvJf /tmp/ptah_agent_latest.tar.xz -C \$HOME/ptah_agent/versions/0_seed
 
 ln -sf \$HOME/ptah_agent/versions/0_seed/ptah_agent \$HOME/ptah_agent/current
 
