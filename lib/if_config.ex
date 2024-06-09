6cd0518350ca78a0eeea33f1f5376c52be518c47
Bohdan Shulha <b.shulha@pm.me>
2024-06-09 20:45:04 +0200

    feat: #27 support various spacing in ifconfig output

--- lib/if_config.ex
+++ lib/if_config.ex
@@ -1,5 +1,8 @@
 defmodule IfConfig do
-  @if_re ~r/^(\w+):\sflags=\w+<(.*)>/
+  require Logger
+  @space_re ~r/\s+/
+
+  @flags_re ~r/^flags=\w+<(.*)>$/
 
   @inet_re ~r/^(inet)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
 
@@ -9,12 +12,12 @@ defmodule IfConfig do
     output =
       String.split(output, "\n")
       |> Enum.filter(&(&1 != ""))
-      |> Enum.map(&String.split(&1, "\t"))
+      |> Enum.map(&Regex.split(@space_re, &1))
 
-    Enum.map(output, &Enum.at(&1, 0))
+    output
     |> Enum.filter(&(&1 != ""))
     |> Enum.map(&parse_if_line/1)
-    |> Enum.filter(&(&1 != nil))
+    |> Enum.reject(&(&1 == nil))
     |> Enum.filter(fn {_, flags} -> Enum.member?(flags, "UP") end)
     |> Enum.filter(fn {_, flags} -> Enum.member?(flags, "RUNNING") end)
     |> Enum.filter(fn {_, flags} -> Enum.member?(flags, "BROADCAST") end)
@@ -24,22 +27,31 @@ defmodule IfConfig do
   end
 
   defp parse_if_line(line) do
-    case Regex.run(@if_re, line) do
-      [_match, if_name, flags] -> {if_name, String.split(flags, ",")}
-      _ -> nil
+    [maybe_if_name, maybe_flags | _] = line
+
+    if String.ends_with?(maybe_if_name, ":") do
+      case Regex.run(@flags_re, maybe_flags) do
+        [_match, flags] ->
+          {maybe_if_name, String.split(flags, ",")}
+
+        _ ->
+          nil
+      end
+    else
+      nil
     end
   end
 
   defp parse_if_data(output, name) do
     index =
       output
-      |> Enum.find_index(fn [if_name | _] -> String.starts_with?(if_name, "#{name}:") end)
+      |> Enum.find_index(fn [if_name | _] -> if_name == name end)
 
     output
     |> Enum.slice((index + 1)..Enum.count(output))
     |> Enum.take_while(fn [if_name | _] -> if_name == "" end)
-    |> Enum.map(fn line -> Regex.run(@inet_re, Enum.join(line, "")) end)
-    |> Enum.filter(&(&1 != nil))
+    |> Enum.map(fn line -> Regex.run(@inet_re, String.trim(Enum.join(line, " "))) end)
+    |> Enum.reject(&(&1 == nil))
     |> Enum.map(fn [_, version, ip] -> {version, ip} end)
   end
 end
